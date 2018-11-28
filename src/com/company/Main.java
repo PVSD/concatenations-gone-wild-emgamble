0f8591c3758ace35e8f7d035b2735085619b13c1
eg913 <eg913@pvsd.org>
2018-11-28 11:08:19 -0500

    i did it!!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,8 +10,13 @@ public class Main {
         System.out.println(t2);
     }
 
+
     private static String cat(int start, int end)
     {
-        //Code goes here
+        StringBuffer sb = new StringBuffer();
+        for (int i = start; i <= end; i++){
+            sb.append((char) i);
+        }
+        return sb.toString();
     }
 }
